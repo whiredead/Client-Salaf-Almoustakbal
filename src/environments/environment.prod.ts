d43860bf679731b0701d9baaf66a46697b522930
El hachmy BELMOUDDEN <h.belmoudden@Alexsys.solutions>
2026-01-27 10:51:07 +0100

    ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/',  // This will use http://safm.duckdns.org/api
+  appUrl: '/api/',  // This will use http://safm.duckdns.org/api
   userKey: 'identityAppUser'
 
 };
